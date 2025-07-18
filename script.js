68fb80f38c626fcb1faed9da70de38c0b6ca026e
Danish Iqbal <di41141455@gmail.com>
2025-07-18 12:49:56 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,90 @@
 
-let taskList =[];
+
+
+let nameList =[];
+let eMailList = [];
 let updateindex;
-let name = document.getElementById('Name');
+let userName = document.getElementById('Name');
 let eMail = document.getElementById('email');
 let addUser = document.getElementById('add');
 let List = document.getElementById('list');
 let updateUser = document.getElementById('updatetask');
 addUser.addEventListener('click' , function(){
-    taskList.push(name.value + eMail.value);
-    console.log(taskList);
-    name.value = '';
+
+    function checkAddInputs (){
+            if (userName.value.trim() === '' || eMail.value.trim() === '' )
+            {
+                addUser.disabled = true;
+            }
+            else {
+                addUser.disabled = false;
+            }
+
+    }
+    if (userName.value.trim() === '' || eMail.value.trim() === '')
+    {
+        //alert('please fill all fields');
+    };
+
+
+    nameList.push(userName.value);
+    eMailList.push(eMail.value);
+    console.log(nameList,eMailList);
+    userName.value = '';
     eMail.value = '';
 
     List.innerHTML='';
-    for (let index = 0; index < taskList.length; index++) {
-        List.innerHTML+=`<li>${taskList[index]}
+    for (let index = 0; index < nameList.length; index++) {
+        List.innerHTML+=`<li>${nameList[index]} ${eMailList[index]}
         <button onclick="deletetask('${index}')">delete</button>
         <button onclick="Edittask('${index}')">Edit</button>
         </li>`;
-     
+        
  }
 });
 
 
 function deletetask(index){
-    taskList.splice(index, 1);
-
+    nameList.splice(index, 1);
+    eMailList.splice(index,1);
     List.innerHTML='';
-    for (let index = 0; index < taskList.length; index++) {
-       List.innerHTML+=`<li>${taskList[index]}
+    for (let index = 0; index < nameList.length; index++) {
+       List.innerHTML+=`<li>${nameList[index]}${eMailList[index]}
         <button onclick="deletetask('${index}')">delete</button>
         </li>`;
-     
     }
- 
- }   
+    Edittask.style.visibility= 'visible';
+ };  
  function Edittask(index){
     //alert(`would you like to Edit task: ${taskList[index]}`);
-    name.value = taskList[index]
-    eMail.value = taskList[index];
-    //eMail.value=taskList[index];
+    userName.value = nameList[index]
+    eMail.value = eMailList[index];
     updateindex=index;
-    
-}
+    onclick = updateUser.style.visibility= 'visible';
+    addUser.disabled= true;
+};
 
 
 updateUser.addEventListener('click' , function(){
  
-    taskList[updateindex]=name.value+eMail.value;
+    nameList[updateindex]=userName.value;
+    eMailList[updateindex]=eMail.value;
+
  
     List.innerHTML='';
-     for (let index = 0; index < taskList.length; index++) {
-         List.innerHTML+=`<li>${taskList[index]}
+     for (let index = 0; index < nameList.length; index++) {
+         List.innerHTML+=`<li>${nameList[index]} ${eMailList[index]}
            <button onclick="deletetask('${index}')">delete</button>
            <button onclick="Edittask('${index}')">Edit</button>
         </li>`;
-         name.value = '';
+         userName.value = '';
          eMail.value = '';
      }
- })
+ });
+
+updateUser.addEventListener('click' , function(){
+
+        this.style.visibility= 'hidden';
+        addUser.disabled= false;
+
+});
